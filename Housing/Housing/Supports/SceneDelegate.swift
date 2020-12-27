203b2cf051ff2ec743551096e01db38b82daba25
junhyeon <oh.junhyeon95@gmail.com>
2020-12-28 00:48:31 +0900

    :bug: ViewController 수정

--- Housing/Housing/Supports/SceneDelegate.swift
+++ Housing/Housing/Supports/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 		
 		let window = UIWindow(windowScene: windowScene)
-		window.rootViewController = ViewController() // Your initial view controller.
+		window.rootViewController = SplashViewController() // Your initial view controller.
 		window.makeKeyAndVisible()
 		self.window = window
 	}
