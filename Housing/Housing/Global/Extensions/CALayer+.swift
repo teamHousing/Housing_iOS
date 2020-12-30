8a23d488b3c81cf4618df9b0f9af508db52d0b36
junhyeon <oh.junhyeon95@gmail.com>
2020-12-30 16:19:58 +0900

    :sparkles: shadow extension 추가

--- Housing/Housing/Global/Extensions/CALayer+.swift
+++ Housing/Housing/Global/Extensions/CALayer+.swift
@@ -5,4 +5,19 @@
 //  Created by 오준현 on 2020/12/29.
 //
 
-import Foundation
+import UIKit
+
+extension CALayer {
+	func applyShadow(
+		color: UIColor = .black,
+		alpha: Float = 0.5,
+		x: CGFloat = 0,
+		y: CGFloat = 10,
+		blur: CGFloat = 4
+	) {
+		shadowColor = color.cgColor
+		shadowOpacity = alpha
+		shadowOffset = CGSize(width: x, height: y)
+		shadowRadius = blur / 1.0
+	}
+}
