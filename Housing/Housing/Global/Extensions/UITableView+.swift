8055c3b8c21915a4cbbfdb7bf256aba9b38e1dc5
junhyeon <oh.junhyeon95@gmail.com>
2020-12-30 16:20:58 +0900

    :sparkles: tableView 설정

--- Housing/Housing/Global/Extensions/UITableView+.swift
+++ Housing/Housing/Global/Extensions/UITableView+.swift
@@ -1,8 +1,19 @@
 //
-//  UITableView.swift
+//  UITableView+.swift
 //  Housing
 //
 //  Created by 오준현 on 2020/12/29.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+	func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+		guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier,
+																				 for: indexPath) as? T else {
+			fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+		}
+		
+		return cell
+	}
+}
