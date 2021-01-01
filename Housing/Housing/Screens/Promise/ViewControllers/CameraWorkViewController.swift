f89a2ec125fa84f2cab2c39e72772a54cb3e9084
st939823 <st939823@gmail.com>
2021-01-01 21:12:57 +0900

    :construction: 카메라로 사진 가져오는 기능
    UIImagePicker 사용하였다. 사진 보관함은 라이브러리 써서 구현하면 이쁠듯

--- Housing/Housing/Screens/Promise/ViewControllers/CameraWorkViewController.swift
+++ Housing/Housing/Screens/Promise/ViewControllers/CameraWorkViewController.swift
@@ -6,50 +6,47 @@
 //
 
 import UIKit
+import Photos
 import AVFoundation
-class CameraWorkViewController: UIViewController {
+class CameraWorkViewController: UIViewController{
 	
 	let sessionn = AVCaptureSession()
-	
 	var camera : AVCaptureDevice?
 	var cameraPreViewLayer : AVCaptureVideoPreviewLayer?
 	var cameraCaptureOutput : AVCapturePhotoOutput?
 	
+	
+	let imagePickerController = UIImagePickerController()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		initializeCapture()
+		
+		imagePickerController.delegate = self
+
+		print(evidencePictures)
 		// Do any additional setup after loading the view.
 	}
-	func initializeCapture() {
-		sessionn.sessionPreset = AVCaptureSession.Preset.high
-		camera = AVCaptureDevice.default(for: .video)
-		
-		do {
-			let cameraCaptureInput = try AVCaptureDeviceInput(device: camera!)
-			cameraCaptureOutput = AVCapturePhotoOutput()
-			sessionn.addInput(cameraCaptureInput)
-			sessionn.addOutput(cameraCaptureOutput!)
-		} catch {
-			print(error.localizedDescription)
+  @IBAction func addPictureFromCamera(_ sender: Any) {
+    self.imagePickerController.sourceType = .camera
+    self.present(self.imagePickerController, animated: true, completion: nil)
+  }
+  @IBAction func addPictureFromLibrary(_ sender: Any) {
+    self.imagePickerController.sourceType = .photoLibrary
+    self.present(imagePickerController, animated: true, completion: nil)
+  }
+  
+	var evidencePictures : [UIImage] = []
+	
+}
+extension CameraWorkViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		//사진 정보
+    print(picker.sourceType)
+		if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+			print("이미지받아왔ㅆ음")
+			evidencePictures.append(image)
 		}
-		
-		cameraPreViewLayer = AVCaptureVideoPreviewLayer(session: sessionn)
-		cameraPreViewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-		cameraPreViewLayer?.frame = view.bounds
-		cameraPreViewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
-		
-		view.layer.insertSublayer(cameraPreViewLayer!, at: 0)
-		sessionn.startRunning()
+    print(evidencePictures)
+		dismiss(animated: true, completion: nil)
 	}
-	
-	/*
-	// MARK: - Navigation
-	
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-	// Get the new view controller using segue.destination.
-	// Pass the selected object to the new view controller.
-	}
-	*/
-	
 }
