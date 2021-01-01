0434f72602845e40a3fa906422f9232fd6b37b07
st939823 <st939823@gmail.com>
2021-01-01 23:27:40 +0900

    :construction: selected images의 thumbnail 가져오기
    카메라로 찍은거 리사이징이 필요됨

--- Housing/Housing/Screens/Promise/ViewControllers/CameraWorkViewController.swift
+++ Housing/Housing/Screens/Promise/ViewControllers/CameraWorkViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import Photos
 import AVFoundation
+import AssetsPickerViewController
+
 class CameraWorkViewController: UIViewController{
 	
 	let sessionn = AVCaptureSession()
@@ -17,25 +19,64 @@ class CameraWorkViewController: UIViewController{
 	
 	
 	let imagePickerController = UIImagePickerController()
+	let picker = AssetsPickerViewController()
 	
+	var thumbnailPictures : [UIImage] = []
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		imagePickerController.delegate = self
-
-		print(evidencePictures)
+		picker.pickerDelegate = self
+		print(thumbnailPictures)
 		// Do any additional setup after loading the view.
 	}
   @IBAction func addPictureFromCamera(_ sender: Any) {
-    self.imagePickerController.sourceType = .camera
-    self.present(self.imagePickerController, animated: true, completion: nil)
+		switch PHPhotoLibrary.authorizationStatus() {
+		case .denied:
+			break
+		case .restricted:
+			break
+		case .authorized:
+			self.imagePickerController.sourceType = .camera
+			self.present(self.imagePickerController, animated: true, completion: nil)
+		case .notDetermined:
+			PHPhotoLibrary.requestAuthorization{
+				state in
+				if state == .authorized {
+					self.imagePickerController.sourceType = .camera
+					self.present(self.imagePickerController, animated: true, completion: nil)
+				}
+				else {
+					self.dismiss(animated: true, completion: nil)
+				}
+			}
+		default:
+			break
+		}
   }
   @IBAction func addPictureFromLibrary(_ sender: Any) {
-    self.imagePickerController.sourceType = .photoLibrary
-    self.present(imagePickerController, animated: true, completion: nil)
+		switch PHPhotoLibrary.authorizationStatus() {
+		case .denied:
+			break
+		case .restricted:
+			break
+		case .authorized:
+			self.present(self.picker, animated: true, completion: nil)
+		case .notDetermined:
+			PHPhotoLibrary.requestAuthorization{
+				state in
+				if state == .authorized {
+					self.present(self.picker, animated: true, completion: nil)
+				}
+				else {
+					self.dismiss(animated: true, completion: nil)
+				}
+			}
+		default:
+			break
+		}
   }
-  
-	var evidencePictures : [UIImage] = []
 	
 }
 extension CameraWorkViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -43,10 +84,29 @@ extension CameraWorkViewController : UIImagePickerControllerDelegate, UINavigati
 		//사진 정보
     print(picker.sourceType)
 		if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-			print("이미지받아왔ㅆ음")
-			evidencePictures.append(image)
+			var thumbnail = UIImage()
+			image.
+			thumbnailPictures.append(image)
 		}
-    print(evidencePictures)
 		dismiss(animated: true, completion: nil)
 	}
+}
+extension CameraWorkViewController : AssetsPickerViewControllerDelegate {
+	func assetsPicker(controller: AssetsPickerViewController, selected assets: [PHAsset]) {
+		for items in assets {
+			thumbnailPictures.append(items.image(targetSize: CGSize(width: 40, height: 40), contentMode: .aspectFill, options: .none))
+		}
+		
+		dismiss(animated: true, completion: nil)
+	}
+}
+extension PHAsset {
+		func image(targetSize: CGSize, contentMode: PHImageContentMode, options: PHImageRequestOptions?) -> UIImage {
+				var thumbnail = UIImage()
+				let imageManager = PHCachingImageManager()
+				imageManager.requestImage(for: self, targetSize: targetSize, contentMode: contentMode, options: options, resultHandler: { image, _ in
+						thumbnail = image!
+				})
+				return thumbnail
+		}
 }
