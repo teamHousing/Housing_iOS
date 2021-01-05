f2463039f1bc299bb227b48d4c9860f80313a492
HelloG <zxasd156@icloud.com>
2021-01-06 04:15:56 +0900

    :construction: 로그인 뷰 레이아웃 진행중
    로그인 뷰 레이아웃 진행중

---
+++ Housing/Housing/Screens/Login/ViewControllers/LoginNavigationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginNavigationViewController.swift
+//  Housing
+//
+//  Created by 곽기곤's Mac on 2021/01/06.
+//
+
+import UIKit
+
+class LoginNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
--- Housing/Housing/Screens/Login/ViewControllers/LoginViewController.swift
+++ Housing/Housing/Screens/Login/ViewControllers/LoginViewController.swift
@@ -9,9 +9,49 @@ import UIKit
 
 class LoginViewController: UIViewController {
 	
+    //MARK:- Component
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var LoginButtonView: UIButton!
+    
+    //MARK:- Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		
+        navigationController?.navigationBar.isHidden = true
+        
+        initLayout()
 	}
+    
+    //MARK:- Helper
+    func initLayout() {
+        idTextField.borderWidth = 1
+        idTextField.borderColor = .white
+        idTextField.tintColor = UIColor(red: 255 / 255, green: 133 / 255, blue: 119 / 255, alpha: 1)
+        
+        passwordTextField.borderWidth = 1
+        passwordTextField.borderColor = .white
+        passwordTextField.tintColor = UIColor(red: 255 / 255, green: 133 / 255, blue: 119 / 255, alpha: 1)
+        
+        LoginButtonView.backgroundColor = UIColor(red: 219 / 255, green: 219 / 255, blue: 219 / 255, alpha: 1)
+        LoginButtonView.layer.cornerRadius = 0.5 * LoginButtonView.bounds.size.height
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+
+         self.view.frame.origin.y = 0
+    }
+
+    @objc func keyboardWillHide(_ sender: Notification) {
+
+        self.view.frame.origin.y = 0
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+
+          self.view.endEditing(true)
+    }
 }
