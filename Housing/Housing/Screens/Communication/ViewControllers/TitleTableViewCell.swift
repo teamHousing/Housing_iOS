16829a8794b1d68eadb6f34fb8a9801031643634
JUBY <uon9816@naver.com>
2021-01-06 04:07:33 +0900

    :construct: 테이블 뷰 동작

--- Housing/Housing/Screens/Communication/ViewControllers/TitleTableViewCell.swift
+++ Housing/Housing/Screens/Communication/ViewControllers/TitleTableViewCell.swift
@@ -13,6 +13,8 @@ class TitleTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+	
+	
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
