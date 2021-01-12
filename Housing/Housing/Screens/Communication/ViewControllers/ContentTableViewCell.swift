eaba43daf3b39ffde0e294754224fccc71cf30c7
JUBY <uon9816@naver.com>
2021-01-12 16:31:25 +0900

    🔀:문의하기버튼문제해결, 코드정리

--- Housing/Housing/Screens/Communication/ViewControllers/ContentTableViewCell.swift
+++ Housing/Housing/Screens/Communication/ViewControllers/ContentTableViewCell.swift
@@ -18,23 +18,22 @@ class ContentTableViewCell: UITableViewCell {
 	
 	var contentData:DetailData?
 	
-
 	func determineProgress(progress: Int) -> String {
 		if progress == 0{
 			return "확인전"
 		} else if progress == 1{
 			return "확인중"
 		} else if progress == 2{
-			return "확인완료"
+			return "해결 완료"
 		}
 		return ""
 	}
 	
 	func filloutCell(){
-//		categoryLabel?.text = contentData?.category
-//		titleLabel?.text = contentData?.issueTitle
-////		statusLabel?.text = determineProgress(progress: contentData?.progress)
-//		contentLabel?.text = contentData?.issueContents
+		categoryLabel?.text = contentData?.category
+		titleLabel?.text = contentData?.issueTitle
+		statusLabel?.text = determineProgress(progress: contentData!.progress)
+		contentLabel?.text = contentData?.issueContents
 	}
 	func makeViewRounded(){
 		whiteUIView.setRounded(radius: 16)
