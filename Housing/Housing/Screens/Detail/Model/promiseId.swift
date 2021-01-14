20d4010c47d9fa587127f6ce8faf74a19f32a93b
st939823 <st939823@gmail.com>
2021-01-14 23:22:27 +0900

    :bug: .

---
+++ Housing/Housing/Screens/Detail/Model/promiseId.swift
@@ -0,0 +1,14 @@
+//
+//  promiseId.swift
+//  Housing
+//
+//  Created by 김태훈 on 2021/01/14.
+//
+
+import Foundation
+import UIKit
+class promiseId {
+	static let shared = promiseId()
+	var id = 1
+	private init() {}
+}
