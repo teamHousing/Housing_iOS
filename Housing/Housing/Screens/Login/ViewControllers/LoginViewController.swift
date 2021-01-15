1d9c2b7067fd7149fd18d4a28b87623648708f1c
junhyeon <oh.junhyeon95@gmail.com>
2021-01-15 21:41:14 +0900

    :ambulance: 로그인 이슈 해결

--- Housing/Housing/Screens/Login/ViewControllers/LoginViewController.swift
+++ Housing/Housing/Screens/Login/ViewControllers/LoginViewController.swift
@@ -100,7 +100,7 @@ final class LoginViewController: BaseViewController {
 		userProvider.rx.request(.signin(email: idTextField.text ?? "",
 																		password: passwordTextField.text ?? ""))
 			.asObservable()
-			.subscribe { [weak self] (next) in
+			.subscribe { (next) in
 				if next.statusCode == 200 {
 					guard let token = (next.response?.allHeaderFields["Set-Cookie"] ?? "") as? String else {
 						return
@@ -125,30 +125,32 @@ final class LoginViewController: BaseViewController {
 						
 						KeychainWrapper.standard.set(cookie,
 																				 forKey: KeychainStorage.accessToken)
-						if ((self?.isSignOut) != nil) {
-							self?.dismiss(animated: true, completion: nil)
+						if self.isSignOut {
+							self.dismiss(animated: true, completion: nil)
 						} else {
 							let viewcontroller = TabBarViewController()
 							viewcontroller.modalPresentationStyle = .fullScreen
-							self?.present(viewcontroller, animated: true)
+							self.present(viewcontroller, animated: true)
 						}
 					} catch {
 						print(error)
 					}
 				}
 				else if next.statusCode == 400 {
-					let alert = UIAlertController(title : "로그인 실패", message: "아이디나 비밀번호가 일치하지 않습니다.", preferredStyle: .alert)
+					let alert = UIAlertController(title : "로그인 실패",
+																				message: "아이디나 비밀번호가 일치하지 않습니다.",
+																				preferredStyle: .alert)
 					let action = UIAlertAction(title: "확인", style: .cancel) {(action) in
-						self?.idTextField.text = nil
-						self?.passwordTextField.text = nil
-						self?.LoginButton.backgroundColor = UIColor(red: 219 / 255,
+						self.idTextField.text = nil
+						self.passwordTextField.text = nil
+						self.LoginButton.backgroundColor = UIColor(red: 219 / 255,
 																											 green: 219 / 255,
 																											 blue: 219 / 255,
 																											 alpha: 1)
-						self?.LoginButton.isEnabled = false
+						self.LoginButton.isEnabled = false
 					}
 					alert.addAction(action)
-					self?.present(alert, animated: false, completion: nil)
+					self.present(alert, animated: false, completion: nil)
 				}
 			} onError: { (error) in
 				print(error.localizedDescription)
